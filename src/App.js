6a1d90fd6193c5f61ae27959317d88996a42deb4
Yaroslav <doctor355@yahoo.com>
2019-07-05 15:18:10 +0300

    Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import Button from './Components/Sandbox'
+import Sandbox from './Components/Sandbox';
 
 const App = () => {
   return (
     <div>
       App
+      <Sandbox />
     </div>
   )
 }
