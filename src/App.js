92eb0da13a0fab860f715cd1a1b2bcca30ebd608
Yaroslav <doctor355@yahoo.com>
2019-07-06 10:12:16 +0300

    add separate folder for component Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 // import Button from './Components/Sandbox'
-import Sandbox from './Components/Sandbox';
+import Sandbox from './Components/Button/Sandbox';
 
 const App = () => {
   return (
     <div>
-      App
       <Sandbox />
     </div>
   )
